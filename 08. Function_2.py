1e3e18f05eefdf1af8a26d26c6f76262ab83f2c2
Juzer Shakir <juz.shakir5253@gmail.com>
2018-12-28 08:45:43 +0530

    New exercise

---
+++ 08. Function_2.py
@@ -0,0 +1,41 @@
+# Exercise 8
+
+"""Write a program to prompt for a score between 0.0 and 1.0. If the score is out of range, print an error message.
+    If the score is between 0.0 and 1.0, print a grade using the following table:
+     
+    Score   Grade
+    >= 0.9     A
+    >= 0.8     B
+    >= 0.7     C
+    >= 0.6     D
+    < 0.6      F            """
+
+# checking for appropriate numeric input
+try:
+  score = float(input('Enter score: '))
+# if not, quit!
+except:
+  print('Bad score')
+  quit()
+
+def computegrade(score):
+  # if input is between 0.0 and 1.0 then print grades..
+  if score >= 0.0 and score <= 1.0:
+    if score >= 0.9:
+      print('A')
+    elif score >= 0.8:
+      print('B')
+    elif score >= 0.7:
+      print('C')
+    elif score >= 0.6:
+      print('D')
+    elif score < 0.6:
+      print('F')
+
+  # else, out of range
+  else:
+    print('Bad score.')
+
+computegrade(score)
+
+# THE END

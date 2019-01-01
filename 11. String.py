e9182e0ee45099d23374f0ee20c8ba1ac55880ba
Juzer Shakir <juz.shakir5253@gmail.com>
2019-01-01 20:06:56 +0530

    New Exercise

---
+++ 11. String.py
@@ -0,0 +1,17 @@
+# Exercise 11
+
+"""  str = 'X-DSPAM-Confidence: 0.8475'
+
+    Use find and string slicing to extract the portion of the string after the colon character 
+    and then use the float function to convert the extracted string into a floating point number.
+"""
+
+str = 'X-DSPAM-Confidence: 0.8475'
+
+pos = str.find(':')
+
+num = str[pos + 2 :]
+
+print(float(num))
+
+# THE END

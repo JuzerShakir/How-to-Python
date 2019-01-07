0b99c25c2949cdaebaa70259aa5e473643bb058a
Juzer Shakir <juz.shakir5253@gmail.com>
2019-01-07 21:39:36 +0530

    New Exercise

---
+++ 15. Dictionary/Most_email.py
@@ -0,0 +1,34 @@
+# Exercise 15
+
+""" Write a program to read through a mail log, build a histogram using a dictionary to count how many messages have come from each email address. 
+    Look through the dictionary using a maximum loop to find who has the most messages and print how many messages the person has."""
+
+name = input("Enter file name: ")
+
+# opens file
+file = open(name)
+
+# empty dictionary for counting
+counts = dict()
+
+# looping through each line in file
+for line in file:
+    # interested in lines that start with From
+    if line.startswith('From'):
+        words = line.split()
+        # two 'From's for single message, ignoring duplicate counts
+        if len(words) > 2:
+            counts[words[1]] = counts.get(words[1], 0) + 1
+
+
+email = None
+count = None
+
+# looping through each key, value in dictionary
+for key, value in counts.items():
+    # email of the person with most messages and its count
+    if count is None or value > count:
+        email = key
+        count = value
+
+print(email, count)
